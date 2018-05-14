430c59c19ca39445f0516f812110073c19a23b44
Ryan Clogg <ryan.clogg@gmail.com>
2018-05-14 17:18:26 +0800

    Banner looking good - cards not so - form not so. Design is rough

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,37 @@
 class DosesController < ApplicationController
+
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    # @cocktail = Cocktail.new
+    # @ingredient = Ingredient.new
+  end
+
+  def create
+    id_ingredient = params["dose"]["ingredient_id"].to_i
+    @dose = Dose.new(dose_params)
+    @dose.ingredient = Ingredient.find(id_ingredient)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:ingredient_id, :cocktail_id, :quantity, :description)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,13 @@ Rails.application.routes.draw do
   root to: 'cocktails#index'
   #COCKTAILS
   get 'cocktails', to: 'cocktails#index', as: 'cocktails'
-  get 'cocktails/:id', to: 'cocktails#show', as: 'cocktail'
 
   get 'cocktails/new', to: 'cocktails#new', as: 'new_cocktail'
   post 'cocktails', to: 'cocktails#create'
+
+  get 'cocktails/:id', to: 'cocktails#show', as: 'cocktail'
+
+  delete 'cocktails/:id',to: 'cocktails#destroy', as: 'destroy_cocktail'
 
   #DOSES
   resources :cocktails do
