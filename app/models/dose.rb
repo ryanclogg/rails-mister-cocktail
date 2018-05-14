94b729720b3d7b6a8db241919447a0506389ad46
Ryan Clogg <ryan.clogg@gmail.com>
2018-05-14 11:37:37 +0800

    Validations complete in model, generated controllers

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,11 @@
 class Dose < ApplicationRecord
   belongs_to :ingredient
   belongs_to :cocktail
+
+  validates_presence_of :ingredient
+  validates_presence_of :cocktail
+  validates_presence_of :description
+
+  validates_uniqueness_of :cocktail, :scope => [:ingredient]
+
 end
